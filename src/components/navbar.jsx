1a02d5fdcf0c60652630aee4a12cded8a4306c46
Eli Sheftolovich <elishafto@gmail.com>
2021-01-22 16:24:13 +0200

    Navbar added

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() { 
+        return ( 
+            <nav className="nav navbar-light bg-light">
+                <a className="nav-link " href="#">NavBar</a>
+            </nav>
+         );
+    }
+}
+ 
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Counter from './components/counter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
-import Counters from './components/counters';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counters/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
